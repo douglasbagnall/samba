64fd9d1be03275b5b6379b2d92d52aca4aefca7c
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2017-06-29 16:49:34 +1200

    traffic_report: add a summary parser

--- python/samba/emulate/traffic_report.py
+++ python/samba/emulate/traffic_report.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 Datapoint = namedtuple('Datapoint',
                        'time, c_id, proto, op_id, duration, success, error')
 
+
 def parser_v1(line):
     line = line.rstrip('\n')
     time, c_id, proto, op_id, duration, success, error = line.split('\t', 6)
@@ -17,10 +18,40 @@ def parser_v1(line):
     return Datapoint(time, c_id, proto, op_id, duration, success, error)
 
 
-
 TIMING_FILE_PARSERS = {
     'time\tconv\tprotocol\ttype\tduration\tsuccessful\terror\n' : parser_v1
 }
 
+
 def get_file_parser(line):
     return TIMING_FILE_PARSERS[line]
+
+SummaryLine = namedtuple('SummaryLine',
+                         'time, ip_protocol, stream_id, src, dest, '
+                         'proto, op_id, desc, has_extra')
+
+def summary_parser(line):
+    fields = line.rstrip('\n').split('\t')
+    (timestamp,
+     ip_protocol,
+     stream_number,
+     src,
+     dest,
+     protocol,
+     opcode,
+     desc) = fields[:8]
+
+    timestamp = float(timestamp)
+    src = int(src)
+    dest = int(dest)
+    has_extra = len(fields) > 8
+    success = success == 'True'
+
+    return SummaryLine(float(timestamp),
+                       ip_protocol,
+                       stream_number,
+                       int(src),
+                       int(dest),
+                       protocol,
+                       op_id,
+                       desc, has_extra)
