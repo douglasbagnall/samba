d12a068bc9ab3f67fffc38c35c2b86b48aebdbc6
Douglas Bagnall <douglas@halo.gen.nz>
2017-07-23 11:43:52 +1200

    copyright for traffic_report module

--- python/samba/emulate/traffic_report.py
+++ python/samba/emulate/traffic_report.py
@@ -1,4 +1,25 @@
 # -*- encoding: utf-8 -*-
+# Functions for generating reports from the traffic emulator results.
+#
+# Copyright (C) Catalyst IT Ltd 2017
+#
+# Catalyst's contribution mostly by Douglas Bagnall
+# <douglas.bagnall@catalyst.net.nz>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from collections import namedtuple
 
 
